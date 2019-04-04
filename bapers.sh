19b08558beb79bd7f60ba3f76b8eaa2b82fadae2
Nii-sanHaxor <45889833+arifarhan16@users.noreply.github.com>
2019-04-04 10:47:11 +0700

    Create bapers.sh

---
+++ bapers.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+
+
+
+clear
+clear
+echo "=============================";
+echo "  TOOLS MANTAN :v ";
+echo "-----------------------------";
+echo " Coded By : Nii-san Haxor    ";
+echo " Team  : IndoSec | BekasiSec ";
+echo " Since : 2017 "; 
+echo "==============================";  
+echo "Lanjut ? [ y/n ]"; read yagak;
+if [ "$yagak" = 'y' ]; then
+clear
+echo "Hei :)";
+sleep 2;
+clear
+echo "Ingat Gak Dulu Kita Pernah Bahagia Bersama :'D ";
+clear
+sleep 4;
+echo "Bercanda Bersama Dibawah Terik Matahari :) ";
+clear
+sleep 4;
+echo "Aku Terlalu Menyayangimu :_) ";
+sleep 3;
+clear
+echo "Siapa Nama Mantanmu ? "; read jawab;
+clear
+sleep 4;
+clear
+echo "Disini Aku Hanya Bisa Mengenangmu :D ";
+sleep 4;
+clear
+echo "Kau Tak Tahu Rasa Sakit Hati Yang Kurasakan Selama Ini :') ";
+sleep 4;
+clear
+echo "Selamat Tinggal $jawab ";
+sleep 4;
+clear
+echo "Semoga Kau Bersama Dia Bisa Bahagia"
+sleep 4;
+clear
+echo "Ingat Jangan Sakiti Dia Seperti Kau Menyakitiku ! :'D ";
+echo "Loading....";
+sleep 8;
+clear
+echo "0.....";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+sleep 0.1;
+clear
+echo ".0....";
+sleep 0.1;
+clear
+echo "..0...";
+sleep 0.1;
+clear
+echo "...0..";
+sleep 0.1;
+clear
+echo "....0.";
+sleep 0.1;
+clear
+echo ".....0";
+clear
+echo "GW MASIH SAYANG SAMA LU, TAPI LU GAK PEKA ANJENGG :v AOWKAKOWAO "
+
+else 
+	clear
+	echo "Lupakan Saja Diriku :) ";
+	echo "Bila Itu Bisa Membuatmu ";
+	echo "Kembali Bersinar Dan Berpijak ";
+	echo "Bacot LO KONTOL :v" ;
+fi 
